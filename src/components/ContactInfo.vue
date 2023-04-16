73e59cb8c99ca63d35491927b6e4b596e66fc67f
jkkfraxy <jkkfraxy@jyu.fi>
2023-04-16 15:23:50 +0300

    contact updates

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="abstract-div contact-info-div" id="contact-info">
+        <h2>Bio</h2>
+        <p>
+            Hello! I am Milla, a wedding and family photographer located in Helsinki, Finland. <br />
+            I love real emotions and pure bright colours. <br />
+            Helping happy couples document their special day since 2010. <br />
+            Let’s make magic happen together. <br />
+            My services are available in English, Finnish and Russian languages.</p>
+        <InstagramLogo />
         <h2>Contact Me</h2>
         <p>TODO: Mailing service</p>
-        <!-- TODO: Instagram feed -->
     </div>
 </template>
 
 <script>
+import InstagramLogo from './InstagramLogo.vue'
 export default {
-    name: 'ContantInfo'
+    name: 'ContantInfo',
+    components: { InstagramLogo }
 }
 </script>
 
